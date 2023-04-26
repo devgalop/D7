3a5d2b4c023d23eb82509ccb452f409aa0f3b203
devgalop <devgalop@gmail.com>
2023-04-26 16:20:21 -0500

    Add DI on Program

--- distrito7.api/Program.cs
+++ distrito7.api/Program.cs
@@ -1,13 +1,17 @@
 using distrito7.api.Data;
 using distrito7.api.Data.Repositories;
 using distrito7.core.Interfaces;
+using distrito7.core.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IRepository,Repository>();
+builder.Services.AddScoped<IRepository, Repository>();
+builder.Services.AddScoped<IMapperService, MapperService>();
+builder.Services.AddScoped<IPaymentPlanService, PaymentPlanService>();
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("D7"));
