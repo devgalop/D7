4fb75f4bcd9b1305dccd3bdcfd5a0d088ab6ae2b
devgalop <devgalop@gmail.com>
2023-04-27 11:01:55 -0500

    Add enviroment variables file

--- distrito7.api/Program.cs
+++ distrito7.api/Program.cs
@@ -1,12 +1,17 @@
+using System.Net;
+using System.ComponentModel;
 using distrito7.api.Data;
 using distrito7.api.Data.Repositories;
 using distrito7.core.Interfaces;
 using distrito7.core.Services;
 using Microsoft.EntityFrameworkCore;
+using dotenv.net;
+using dotenv.net.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+DotEnv.Load();
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddScoped<IMapperService, MapperService>();
 builder.Services.AddScoped<IPaymentPlanService, PaymentPlanService>();
@@ -14,7 +19,11 @@ builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddDbContext<DataContext>(options =>
 {
+    string dbServer = EnvReader.GetStringValue("DBSERVER");
+    string dbName = EnvReader.GetStringValue("DBNAME");
+    string connectionString = $"Server={dbServer};Database={dbName};Trusted_Connection=True;TrustServerCertificate=True;";
     options.UseSqlServer(builder.Configuration.GetConnectionString("D7"));
+    options.UseSqlServer(connectionString);
 });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
